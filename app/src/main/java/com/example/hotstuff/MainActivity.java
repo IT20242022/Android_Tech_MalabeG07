6a0ca06c803172af7ea6328e67eaa83a5e539a34
IT20242022 <IT20242022@my.sliit.lk>
2021-09-25 04:52:59 +0530

    Fourth Commit

--- app/src/main/java/com/example/hotstuff/MainActivity.java
+++ app/src/main/java/com/example/hotstuff/MainActivity.java
@@ -3,21 +3,45 @@ package com.example.hotstuff;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int  CAMERA_REQUEST=1888;
+    ImageView imageView;
+    Button Takephoto;
 
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA_REQUEST){
+            Bitmap photo=(Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imageView=(ImageView) this.findViewById(R.id.imageView) ;
+        Button Takephoto =(Button) this.findViewById(R.id.Takephoto);
+
+        Takephoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent,CAMERA_REQUEST);
+            }
+
+        });
 
 
         final EditText price = findViewById(R.id.price);
